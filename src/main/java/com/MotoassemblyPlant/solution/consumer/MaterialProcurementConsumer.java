00ca8056b06158f0f21d3fed6a9806c9cc694646
dattu-kapli24 <dattukapli@gmail.com>
2023-08-16 19:09:17 +0530

    commit 2 for Producer and kafka configuration

--- src/main/java/com/MotoassemblyPlant/solution/consumer/MaterialProcurementConsumer.java
+++ src/main/java/com/MotoassemblyPlant/solution/consumer/MaterialProcurementConsumer.java
@@ -1,14 +1,15 @@
-package com.MotoassemblyPlant.solution.producer;
+package com.MotoassemblyPlant.solution.consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Service
-public class MaterialProcurementProducer {
+public class MaterialProcurementConsumer {
 
 	@Value("${spring.kafka.topic.name}")
 	private String topicName;
@@ -16,9 +17,10 @@ public class MaterialProcurementProducer {
 	@Autowired
 	private KafkaTemplate<String, String> KafkaTemplate;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(MaterialProcurementProducer.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(MaterialProcurementConsumer.class);
 
-	public void materialProcurementMessage(String message) {
+	@KafkaListener(topics="topicName")
+	public void consumeProcurementMessage(String message) {
 
 		LOGGER.info(String.format("Material Procurement Message %s", message));
 		KafkaTemplate.send(topicName, message);
